978e9630429a77d45207f8945f7da6d450e59126
Fernando Hidemi Uchiyama <fernandoservico@gmail.com>
2020-02-13 17:28:50 -0300

    Code refactory

--- src/Game.js
+++ src/Game.js
@@ -1,60 +1,47 @@
 import React from "react";
 
 import Board from "./Board";
-import calculateWinner from "./functions";
+import { calculateWinner, getPositionFromIndex, highlightWinnerIfAny } from "./functions";
 
 class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = this.getFirstState();
+
+        this.state.reverse = false;
     }
 
     render() {
         return (
-            <div className="game">
-                <div className="game-board">
-                    <Board
-                        squares={this.getCurrentMoveSquares()}
-                        doPlayerMovement={(i) => this.doPlayerMovement(i)}
-                    />
-                    <p>{this.getMoveDescription()}</p>
-                </div>
-                <div className="game-info">
-                    <ol>{this.getMovesList()}</ol>
+            <div>
+                <h1>Hello World - Tic Tac Toe</h1>
+                <div className="game">
+                    <div className="game-board">
+                        <h4>Player 1: X &nbsp;&nbsp; Player 2: O</h4>
+                        <Board
+                            squares={this.getCurrentMoveSquares()}
+                            doPlayerMovement={(i) => this.doPlayerMovement(i)}
+                        />
+                        <p dangerouslySetInnerHTML={{__html: this.getMoveDescription()}}></p>
+                    </div>
+                    <div className="game-info">
+                        <p className="moves-history-title">
+                            <strong>Moves History:</strong>
+                            (<input
+                                type="checkbox"
+                                checked={this.state.reverse}
+                                onChange={this.reverseClick}
+                                id="reverse" />reverse?)
+                        </p>
+                        <ol>{this.getMovesListHistory()}</ol>
+                    </div>
                 </div>
             </div>
         );
     }
 
-    getMovesList() {
-        return this.state.history.map((step, move) => {
-            const desc = move ?
-                'Go to move #' + move :
-                'Go to game start';
-
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpToHistoryMove(move)}>{desc}</button>
-                </li>
-            );
-        });
-    }
-
-    getMoveDescription() {
-        const winner = calculateWinner(this.getCurrentMoveSquares());
-
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + ((this.state.xIsNext) ? 'X' : 'O');
-        }
-
-        return status;
-    }
-
     getCurrentMoveSquares() {
-        return this.state.history[this.state.stepNumber].squares;
+        return highlightWinnerIfAny(this.state.history[this.state.stepNumber].squares, 'square-winner');
     }
 
     doPlayerMovement(i) {
@@ -70,6 +57,7 @@ class Game extends React.Component {
 
         const newState = {
             history: historyCopy.concat({
+                movePosition: getPositionFromIndex(i),
                 squares: currentSquares
             }),
             stepNumber: historyCopy.length,
@@ -77,6 +65,47 @@ class Game extends React.Component {
         };
 
         this.setState(newState);
+    }
+
+    getMoveDescription() {
+        const winner = calculateWinner(this.getCurrentMoveSquares());
+
+        switch(winner) {
+            case 'draw':
+                return '<span class="description winner">Draw: No winner!</span>';
+            case 'O':
+            case 'X':
+                return '<span class="description winner">Winner: ' + (winner == 'X' ? 'Player 1' : 'Player 2') + '</span>';
+            default:
+                return '<span class="description"><u>Next</u>: ' + (((this.state.xIsNext) ? 'Player 1' : 'Player 2') + '</span>');
+        }
+    }
+
+    reverseClick = () => {
+        this.setState({reverse: !this.state.reverse});
+    };
+
+    getMovesListHistory() {
+        let movesHistory = this.state.history.map((step, move, position) => {
+            const desc = move ?
+                'Go to move #' + move + ' (' + step.movePosition + ')':
+                'Go to game start';
+
+            return (
+                <li key={move}>
+                    <button
+                        style={this.state.stepNumber == move ? {fontWeight: 'bold'} : {}}
+                        onClick={() => this.jumpToHistoryMove(move)}
+                    >{desc}</button>
+                </li>
+            );
+        });
+
+        if (this.state.reverse) {
+            return movesHistory.reverse();
+        }
+
+        return movesHistory;
     }
 
     jumpToHistoryMove(step) {
@@ -94,6 +123,7 @@ class Game extends React.Component {
         return {
             history: [blankHistoryItem],
             stepNumber: 0,
+            movePosition: "",
             xIsNext: true
         };
     }
